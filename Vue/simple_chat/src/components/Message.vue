a1c07caaef949dcfebecfb5364b2f8a68e15e531
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-05-09 20:57:37 +0430

    Update Message.vue

--- Vue/simple_chat/src/components/Message.vue
+++ Vue/simple_chat/src/components/Message.vue
@@ -1,11 +1,26 @@
 <template>
+<div>
+    <div class="{{getClass()}}">
+
+    </div>
+</div>
 </template>
 
 <script>
     export default{
         name:"Message",
         props:{
-            message:String
-        }
+            message:String,
+            align:String,
+            hasImage:Boolean,
+            date:String,
+            profileImage:String,
+            profileName:String
+        },
+        methods: {
+          getClass(){
+            return "msg" + align + "-msg"
+          }  
+        },
     }
 </script>
