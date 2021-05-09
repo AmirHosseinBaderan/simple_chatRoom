50ce1dc58b37947a3ae9235d1d7ad8c6da662df0
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-05-09 20:01:02 +0430

    update load image

--- Js/main.js
+++ Js/main.js
@@ -3,11 +3,11 @@ const msgerInput = get(".msger-input");
 const msgerChat = get(".msger-chat");
 
 const BOT_MSGS = [
-  "Karmandet",
-  "NEw Karmandet",
-  "Test Karmandet",
-  "بیخیال ما شو جون خودت",
-  "اپلیکشین ما بیب"
+    "Karmandet",
+    "NEw Karmandet",
+    "Test Karmandet",
+    "بیخیال ما شو جون خودت",
+    "اپلیکشین ما بیب"
 ]
 
 // Icons made by Freepik from www.flaticon.com
@@ -17,30 +17,28 @@ const BOT_NAME = "کارمند";
 const PERSON_NAME = "امیر حسین بادران";
 
 msgerForm.addEventListener("submit", event => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const msgText = msgerInput.value;
-  if (!msgText) return;
+    const msgText = msgerInput.value;
+    let input = document.getElementById('choseFileInput');
+    if (!msgText && !input.value) return;
 
-  let base64 = convertToBase64(document.getElementById('choseFileInput'));
+    appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText, input);
+    msgerInput.value = "";
 
-  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText,base64);
-  msgerInput.value = "";
-
-  botResponse();
+    botResponse();
 });
 
-function appendMessage(name, img, side, text,imgMessage) {
-  //   Simple solution for small apps
-  let image = '';
-  if(imgMessage !== ''){
-    image = `
-    <div class="msg-image">
-      <img src="${imgMessage}"/>
-    </div>
-    `
-  }
-  const msgHTML = `
+function appendMessage(name, img, side, text, input) {
+    let image = '';
+    let id = "id" + Math.random().toString(16).slice(2);
+    if (input.value != '') {
+        image = `
+      <div class="msg-image">
+        <img  id="${id}"/>
+      </div>`
+    }
+    const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
 
@@ -54,49 +52,48 @@ function appendMessage(name, img, side, text,imgMessage) {
       </div>
     </div>
   `;
+    if (image !== '') saveImage(input, id);
 
-  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-  msgerChat.scrollTop += 500;
+    msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+    msgerChat.scrollTop += 500;
 }
 
 function botResponse() {
-  const r = random(0, BOT_MSGS.length - 1);
-  const msgText = BOT_MSGS[r];
-  const delay = msgText.split(" ").length * 100;
+    const r = random(0, BOT_MSGS.length - 1);
+    const msgText = BOT_MSGS[r];
+    const delay = msgText.split(" ").length * 100;
 
-  setTimeout(() => {
-    appendMessage(BOT_NAME, BOT_IMG, "left", msgText,'');
-  }, delay);
+    setTimeout(() => {
+        appendMessage(BOT_NAME, BOT_IMG, "left", msgText, '');
+    }, delay);
 }
 
 // Utils
 function get(selector, root = document) {
-  return root.querySelector(selector);
+    return root.querySelector(selector);
 }
 
 function formatDate(date) {
-  const h = "0" + date.getHours();
-  const m = "0" + date.getMinutes();
+    const h = "0" + date.getHours();
+    const m = "0" + date.getMinutes();
 
-  return `${h.slice(-2)}:${m.slice(-2)}`;
+    return `${h.slice(-2)}:${m.slice(-2)}`;
 }
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-function choseFile(id){
-  let input = document.getElementById(id);
-  input.click();
+function choseFile(id) {
+    let input = document.getElementById(id);
+    input.click();
 }
 
-function convertToBase64(intput){
-  let fileReader = new FileReader();
-  fileReader.readAsDataURL(intput.files[0]);  
-  fileReader.addEventListener("load",function(){
-    console.log('loaded');
-  })
-  // fileReader.onlo = () => {
-  //   return fileReader.result;
-  // };  
+function saveImage(input, imgId) {
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(input.files[0]);
+    fileReader.addEventListener("load", () => {
+        let img = document.getElementById(imgId);
+        img.src = fileReader.result;
+    });
 }
