37974e68182d050fdb236066a39575fdafc3a684
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-05-10 23:06:52 +0430

    insert sidebar vue and css

---
+++ Vue/simple_chat/src/components/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+<script>
+
+export default{
+    name:'sideBar',
+    props:{
+        chats:{
+            title:String,
+            profileImage:String,
+            isOnline:String,
+            shortMessage:String
+        }
+    }
+}
+
+</script>
+
+<style>
+@import "./assets/sidebar.css";
+</style>
